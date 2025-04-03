378a91196fa2c5c7e37e895c83d9694e54e50c8f
hong-seung-kwan <hh8164@naver.com>
2025-04-03 17:28:02 +0900

    d

--- JS 자바스크립트 내장 객체/quiz2/quiz8.js
+++ JS 자바스크립트 내장 객체/quiz2/quiz8.js
@@ -8,6 +8,19 @@ students.set('최지민' ,{grade:'4학년', major: '화학'});
 students.set('정예린' ,{grade:'2학년', major: '수학'});
 students.set('한지우' ,{grade:'3학년', major: '영어영문학'});
 
-for(const student of students){
-  console.log(students.keys.includes(key,'3학년'));
+for(const [a,b] of students){
+  // console.log(a,b);
+  if(b.grade === "3학년"){
+    students.set(a, {grade:b.grade, major:'융합공학'});
+  }
 }
+console.log(students);
+
+// 이름 '정예린' 학생이 있으면 학생 정보 출력
+
+console.log(students.has('정예린'));
+
+if(students.has('정예린')){
+  console.log(students.get('정예린'));
+}
+
